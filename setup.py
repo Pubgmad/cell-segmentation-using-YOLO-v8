8301d3c2efc06fbadc149accc50c9d6ff5fd02c0
BALAJI RL <balajirajlat@gmail.com>
2025-02-27 11:31:33 +0530

    requirements and local packages are initialised

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Balaji RL',
+    author_email= 'balajirajlat@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
